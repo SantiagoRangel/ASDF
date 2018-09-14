1ef7c9d67f7337fbaca138f5b161ee2aa94b13ef
SantiagoRangel <36021620+SantiagoRangel@users.noreply.github.com>
2018-09-14 09:04:12 -0500

    Update consumerN.py

--- consumerN.py
+++ consumerN.py
@@ -1,13 +1,15 @@
 import json, time
-from kafka import KafkaConsumer
+from kafka import KafkaProducer
 from kafka.errors import KafkaError
 from random import uniform
 
-consumer = KafkaConsumer(bootstrap_servers=['ip_kafka_server:kafka_port'],
-                         value_deserializer=lambda m: json.loads(m.decode('utf-8')))
-consumer.subscribe(pattern='chapinero-.*')
+producer = KafkaProducer(bootstrap_servers=['ip_kafka_server:kafka_port'], 
+						 value_serializer=lambda v: json.dumps(v).encode('utf-8'))
 
-for message in consumer:
-    print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
-                                          message.offset, message.key,
-                                          message.value
+while True:
+	producer.send('alta-piso2-cuarto1', {'time': time.strftime("%X"), 'measurement': 'temperature', 
+					'value': round(uniform(20, 25),1), 'unit': 'C', 'place': 'piso2/cuarto1'})
+	producer.send('baja-piso2-cuarto2', {'time': time.strftime("%X"), 'measurement': 'temperature', 
+					'value': round(uniform(20, 25),1), 'unit': 'C', 'place': 'piso2/cuarto2'})
+	producer.flush()
+	time.sleep(5)
